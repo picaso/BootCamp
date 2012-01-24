e0e058031188b4151b8248b1cda2fed95b152cac
picaso <osaide@gmail.com>
2012-01-24 14:44:59 -0600

    Rectangle-Square Class

--- ShapeTest.rb
+++ ShapeTest.rb
@@ -2,8 +2,15 @@ require 'test/unit'
 class  ShapeTest < Test::Unit::TestCase
   def test_area
      rect =  Rectangle.new(6,4)
+    
     assert_equal 24, rect.area
   end
+  
+  def test_square_has_equal_sides
+     square = Square.new(6,6)
+     assert_equal square.length, square.width
+  end
+    
    
 end
 
@@ -20,4 +27,17 @@ class Rectangle
   
 end
 
+
+class Square
+  attr_reader :length, :width
+  
+  def initialize(length, width)
+     @length = length
+     @width = width
+  end
+  def area
+     @length * @width
+  end
+    
+end
 
