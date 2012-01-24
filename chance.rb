65d5cd96747ca086ab3dedd0bb632ea315c0a19c
picaso <osaide@gmail.com>
2012-01-24 16:38:04 -0600

    Rakefile update

--- chance.rb
+++ chance.rb
@@ -1,4 +1,3 @@
-require 'test/unit'
 class Chance
   def initialize(value)
     @occurance = value
@@ -21,29 +20,3 @@ class Chance
   
 end
 
-
-class  ChanceTest < Test::Unit::TestCase
-  def test_probability_you_get_a_head_when_you_flip_a_coin
-     coinChance = Chance.new(50)
-     
-     assert_equal 50, coinChance.get_chance_happening
-  end
-  def test_probability_you_do_not_get_a_head_when_you_flip_a_coin
-     coinChance = Chance.new(50)
-     
-     assert_equal 50, coinChance.get_chance_not_happening
-  end
-  
-  def test_probability_you_get_a_3_when_you_roll_a_dice
-     coinChance = Chance.new(16.67)
-     
-     assert_equal 16.67, coinChance.get_chance_happening
-  end
-  
-  
-  def test_probability_you_do_not_get_a_3_when_you_roll_a_dice
-     coinChance = Chance.new(16.67)
-     
-     assert_equal 100-16.67, coinChance.get_chance_not_happening
-  end
-end
